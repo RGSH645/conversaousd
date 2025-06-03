4a983c9fb4319f464261b31ea666469485d20e1c
RGSH645 <RC4968867@GMAIL.COM>
2025-06-03 15:36:50 -0300

    Add files via upload

---
+++ converção.py
@@ -0,0 +1,12 @@
+num_dolar = float(input("Digite o valor em dolar: "))
+num_real = float(input("Digite o valor em real: "))
+
+conversao_real = num_dolar * 5.64
+conversao_dolar = num_real / 5.64
+
+print(f"seu dolar convertido para real é:{conversao_real: .2f}R$")
+print(f"seu real  convertido para dolar é:{conversao_dolar: .2f}$")
+
+
+
+
